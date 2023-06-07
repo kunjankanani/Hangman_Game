2f26d84809194579132fd7e24043481b0e0fe05b
Kunjan Kanani <115248453+kunjankanani@users.noreply.github.com>
2023-06-07 18:20:03 +0530

    [Add]hangman_game

---
+++ Hangman_Game/hangman_art.py
@@ -0,0 +1,84 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+                                                                                                                                                                         
+                                                                                                                                                                         
+HHHHHHHHH     HHHHHHHHH                                                                                                                                                  
+H:::::::H     H:::::::H                                                                                                                                                  
+H:::::::H     H:::::::H                                                                                                                                                  
+HH::::::H     H::::::HH                                                                                                                                                  
+  H:::::H     H:::::H         aaaaaaaaaaaaa        nnnn  nnnnnnnn            ggggggggg   ggggg        mmmmmmm    mmmmmmm          aaaaaaaaaaaaa        nnnn  nnnnnnnn    
+  H:::::H     H:::::H         a::::::::::::a       n:::nn::::::::nn         g:::::::::ggg::::g      mm:::::::m  m:::::::mm        a::::::::::::a       n:::nn::::::::nn  
+  H::::::HHHHH::::::H         aaaaaaaaa:::::a      n::::::::::::::nn       g:::::::::::::::::g     m::::::::::mm::::::::::m       aaaaaaaaa:::::a      n::::::::::::::nn 
+  H:::::::::::::::::H                  a::::a      nn:::::::::::::::n     g::::::ggggg::::::gg     m::::::::::::::::::::::m                a::::a      nn:::::::::::::::n
+  H:::::::::::::::::H           aaaaaaa:::::a        n:::::nnnn:::::n     g:::::g     g:::::g      m:::::mmm::::::mmm:::::m         aaaaaaa:::::a        n:::::nnnn:::::n
+  H::::::HHHHH::::::H         aa::::::::::::a        n::::n    n::::n     g:::::g     g:::::g      m::::m   m::::m   m::::m       aa::::::::::::a        n::::n    n::::n
+  H:::::H     H:::::H        a::::aaaa::::::a        n::::n    n::::n     g:::::g     g:::::g      m::::m   m::::m   m::::m      a::::aaaa::::::a        n::::n    n::::n
+  H:::::H     H:::::H       a::::a    a:::::a        n::::n    n::::n     g::::::g    g:::::g      m::::m   m::::m   m::::m     a::::a    a:::::a        n::::n    n::::n
+HH::::::H     H::::::HH     a::::a    a:::::a        n::::n    n::::n     g:::::::ggggg:::::g      m::::m   m::::m   m::::m     a::::a    a:::::a        n::::n    n::::n
+H:::::::H     H:::::::H     a:::::aaaa::::::a        n::::n    n::::n      g::::::::::::::::g      m::::m   m::::m   m::::m     a:::::aaaa::::::a        n::::n    n::::n
+H:::::::H     H:::::::H      a::::::::::aa:::a       n::::n    n::::n       gg::::::::::::::g      m::::m   m::::m   m::::m      a::::::::::aa:::a       n::::n    n::::n
+HHHHHHHHH     HHHHHHHHH       aaaaaaaaaa  aaaa       nnnnnn    nnnnnn         gggggggg::::::g      mmmmmm   mmmmmm   mmmmmm       aaaaaaaaaa  aaaa       nnnnnn    nnnnnn
+                                                                                      g:::::g                                                                            
+                                                                          gggggg      g:::::g                                                                            
+                                                                          g:::::gg   gg:::::g                                                                            
+                                                                           g::::::ggg:::::::g                                                                            
+                                                                            gg:::::::::::::g                                                                             
+                                                                              ggg::::::ggg                                                                               
+                                                                                 gggggg                                                                                  
+ '''
